89811d74b3a1c8ed677cc1e4ef214703304b854b
swag1 <swag1@SEROJA>
2023-03-31 18:06:36 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz10_4
+{
+    public partial class MainWindow : Window
+    {
+        List<Notes> notes;
+        List<Notes> activeNotes;
+        List<Notes> completeNotes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            notes = new List<Notes>() {
+            new Notes("погладить кошетчек",Status.Active),
+            new Notes("помяукать",Status.Active),
+             new Notes("получить неаттестацию по мдк",Status.Completed)
+            };
+            Update();
+        }
+
+        private void Online_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (Online.SelectedItem == null) return;
+            var a = notes.FindIndex(u => u == Online.SelectedItem);
+            notes[a].status = Status.Completed;
+            Update();
+        }
+        public void Update()
+        {
+            activeNotes = notes.Where(u => u.status == Status.Active).ToList();
+            completeNotes = notes.Where(u => u.status == Status.Completed).ToList();
+            Online.ItemsSource = activeNotes;
+            Offline.ItemsSource = completeNotes;
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextNotes.Text == "" || TextNotes.Text == null) return;
+            notes.Add(new Notes(TextNotes.Text, Status.Active));
+            Update();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (Offline.SelectedItems == null) return;
+            foreach (var note in Offline.SelectedItems)
+            {
+                notes.Remove((Notes)note);
+            }
+            Update();
+        }
+    }
+}
---
+++ Notes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz10_4
+{
+    internal class Notes
+    {
+        public string Name { set; get; }
+        public Status status { set; get; }
+        public Notes(string name, Status st)
+        {
+            Name = name;
+            status = st;
+        }
+    }
+    public enum Status
+    {
+        Active,
+        Completed
+    }
+}
